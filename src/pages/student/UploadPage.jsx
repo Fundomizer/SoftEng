5ed18450d3d7fc5d441e375365ba683b51bf6986
UbelYes <ubelzz@gmail.com>
2025-10-25 20:44:04 +0800

    Start of student landing page after login. Page for uploading file.

---
+++ src/pages/student/UploadPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+//import './UploadPage.css';
+
+export const UploadPage = () => {
+    return (
+        <section>
+            <div>
+                <nav>
+                    <img src="src\assets\slu_logo.png" alt="SLU-logo" />
+                    <div>
+                        <h3>Student Printing Service Portal</h3>
+                        <span id="current-user">Amogus 69</span>
+                    </div>
+                </nav>
+            </div>
+        </section>
+    );
+};
