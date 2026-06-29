a46f307fef18930fe26c4602e8afee97604cbd2f
Fundomizer <jesusjroway@gmail.com>
2026-06-29 08:56:42 +0800

    fix: Fixed API URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const HOST = `http://${window.location.hostname}`
 const PORT = 3001;
-const API_BASE = `${HOST.replace(/\/$/, '')}:${PORT}`;
+const API_BASE = `${HOST}:${PORT}`;
 
 export { HOST, PORT, API_BASE }
